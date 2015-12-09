8339e8e7ad6441e05c20bcd2cd17bfeed0098078
williamjehanne <william.jehanne@gmail.com>
2015-12-09 09:19:51 +0100

    Initial Commit

---
+++ ZombieApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZombieApp
+//
+//  Created by William JEHANNE on 09/12/2015.
+//  Copyright © 2015 William JEHANNE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
